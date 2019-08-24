1fcd538af820327073b1296252e6a7c13ba281a7
rakesh m r <rakeshmr723@gmail.com>
2019-08-24 11:10:41 +0530

    initial commit

---
+++ script/script.js
@@ -0,0 +1,15 @@
+function  menuopen(){
+    document.querySelector(".menu").style="visibility:hidden";
+    document.querySelector(".op1").style=" display: grid; width: 100%;text-align: center;margin:20px;";
+    document.querySelector(".op2").style=" display: grid; width: 100%;text-align: center;margin:20px;";
+    document.querySelector(".op3").style=" display: grid;width: 100%;text-align: center;margin: 20px;";
+    document.querySelector(".x").style="visibility: visible; display: grid;";
+    document.querySelector(".respmenu").style="background: cyan;";
+}
+function menuclose(){
+    document.querySelector(".menu").style="visibility: visible";
+    document.querySelector(".op1").style="display: none";
+    document.querySelector(".op2").style="display: none";
+    document.querySelector(".op3").style="display: none";
+    document.querySelector(".x").style="visibility: hidden";
+}
