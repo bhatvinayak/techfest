07ccb7014f7e29da352b683869a07810706bcf94
rakesh m r <rakeshmr723@gmail.com>
2019-08-25 10:47:47 +0530

    update

--- script/script.js
+++ script/script.js
@@ -1,8 +1,8 @@
 function  menuopen(){
     document.querySelector(".menu").style="visibility:hidden";
-    document.querySelector(".op1").style=" display: grid; width: 100%;text-align: center;margin:20px;";
-    document.querySelector(".op2").style=" display: grid; width: 100%;text-align: center;margin:20px;";
-    document.querySelector(".op3").style=" display: grid;width: 100%;text-align: center;margin: 20px;";
+    document.querySelector(".op1").style=" display: grid; width: 100%;text-align: center;padding:20px";
+    document.querySelector(".op2").style=" display: grid; width: 100%;text-align: center;padding:20px";
+    document.querySelector(".op3").style=" display: grid;width: 100%;text-align: center;padding:20px";
     document.querySelector(".x").style="visibility: visible; display: grid;";
     document.querySelector(".respmenu").style="background: rgb(219, 160, 32);";
 }
