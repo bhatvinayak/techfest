28ff903df5aa22a144c01d59b555f2d4e9311bb6
rakesh m r <rakeshmr723@gmail.com>
2019-08-26 19:28:34 +0530

    update

--- script/script.js
+++ script/script.js
@@ -15,7 +15,47 @@ function menuclose(){
 }
 function opencse(){
     document.querySelector(".cse").style="display: initial;"
+    document.querySelector(".civ").style="display: none;"
+    document.querySelector(".mec").style="display: none;"
+    document.querySelector(".ece").style="display: none;"
+    document.querySelector(".eee").style="display: none;"
 }
+function openise(){
+    document.querySelector(".cse").style="display: initial;"
+    document.querySelector(".civ").style="display: none;"
+    document.querySelector(".mec").style="display: none;"
+    document.querySelector(".ece").style="display: none;"
+    document.querySelector(".eee").style="display: none;"
+}
+function openece(){
+    document.querySelector(".cse").style="display: none;"
+    document.querySelector(".civ").style="display: none;"
+    document.querySelector(".mec").style="display: none;"
+    document.querySelector(".ece").style="display: initial;"
+    document.querySelector(".eee").style="display: none;"
+}
+function openeee(){
+    document.querySelector(".cse").style="display: none;"
+    document.querySelector(".civ").style="display: none;"
+    document.querySelector(".mec").style="display: none;"
+    document.querySelector(".ece").style="display: none;"
+    document.querySelector(".eee").style="display: initial;"
+}
+function openciv(){
+    document.querySelector(".cse").style="display: none;"
+    document.querySelector(".civ").style="display: initial;"
+    document.querySelector(".mec").style="display: none;"
+    document.querySelector(".ece").style="display: none;"
+    document.querySelector(".eee").style="display: none;"
+}
+function openmec(){
+    document.querySelector(".cse").style="display: none;"
+    document.querySelector(".civ").style="display: none;"
+    document.querySelector(".mec").style="display: initial;"
+    document.querySelector(".ece").style="display: none;"
+    document.querySelector(".eee").style="display: none;"
+}
+
 function register()
 {
     document.querySelector(".reg-form").style="display: initial;";
