c322c2cd667131c68f5666f0f0506c072070beaa
rakesh m r <rakeshmr723@gmail.com>
2019-08-28 17:50:58 +0530

    update

--- script/script.js
+++ script/script.js
@@ -1,18 +1,3 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js')
-        .then(reg=>{
-          console.log('Registered',reg);
-        }).catch(err=>{
-          console.log('Registeration failed',err);
-        });
-    });
-  }
-window.addEventListener('beforeinstallPrompt',(e)=>{
-    e.preventDefault();
-    deferredPrompt=e;
-    btnAdd.style.display='block';
-  });
 
 function  menuopen(){
     document.querySelector(".menu").style="visibility:hidden";
