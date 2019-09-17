4533e30c3359f7fd90aa4de788173743394c8d16
rakesh m r <rakeshmr723@gmail.com>
2019-09-17 21:39:36 +0530

    update

--- script/script.js
+++ script/script.js
@@ -1,3 +1,18 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('./script/sw.js')
+        .then(reg=>{
+          console.log('Registered',reg);
+        }).catch(err=>{
+          console.log('Registeration failed',err);
+        });
+    });
+  }
+window.addEventListener('beforeinstallPrompt',(e)=>{
+    e.preventDefault();
+    deferredPrompt=e;
+    btnAdd.style.display='block';
+  });
 
 function  menuopen(){
     document.querySelector(".menu").style="visibility:hidden";
---
+++ script/sw.js
@@ -0,0 +1,35 @@
+self.addEventListener('install',event=>{
+    console.log('Install evnet');
+});
+self.addEventListener('activate',event=>{
+    console.log('Activate evnet');
+});
+self.addEventListener('fetch',event=>{
+    console.log('Install evnet:',event.request.url);
+});
+const cacheName='cache-v1';
+const resourcesToPrecache=[
+    '/',
+    'index.html',
+    'style.css',
+    'script.js',
+    'app.png',
+    'app1.png',
+    'event.css'
+];
+self.addEventListener('install',event=>{
+    console.log('service worker install event');
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache=>{
+                return cache.addAll(resourcesToPrecache);
+            })
+    );
+});
+self.addEventListener('fetch',event=>{
+    event.respondWith(caches.match(event.request)
+    .then(cachedResponse=> {
+        return cachedResponse||fetch(event.request);
+    })
+    );
+});
